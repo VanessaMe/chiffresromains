bcc46fe5f6a1aad6750bd3797ada8a88d11fdcfa
Vanessa Mézières <vanessa.mezieres@etudiant.univ-lille1.fr>
2016-04-22 14:36:24 +0200

    chiffreII

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -3,11 +3,27 @@ package iut.tdd;
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
 		String nombre = "";
-		if(arabe == "1"){
+		
+		Integer nb = Integer.parseInt(arabe);
+		
+		while(nb > 0){
+			if(nb/5 > 0){
+				nombre += "V";
+				nb = nb - 5;
+			} 
+			
+			if(nb/1 > 0){
+				nombre += "I";
+				nb = nb - 1;
+			}
+		}
+		
+		
+		/*if(arabe == "1"){
 			nombre = "I";
 		} else if(arabe == "5"){
 			nombre = "V";
-		}
+		} */
 		
 		return nombre;
 		
--- src/test/java/iut/tdd/RomanNumeralsTest.java
+++ src/test/java/iut/tdd/RomanNumeralsTest.java
@@ -18,7 +18,19 @@ public class RomanNumeralsTest {
 	}
 	
 	@Test
-	public void sould_return_V_when_5 (){
+	public void should_return_II_when_2 (){
+		//Given
+		String input = "2";
+		String expected = "II";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		//When
+		String actual = romanNumeral.convertToRoman(input);
+		//Then
+		Assert.assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void should_return_V_when_5 (){
 		//Given
 		String input = "5";
 		String expected = "V";
@@ -31,7 +43,7 @@ public class RomanNumeralsTest {
 	
 	
 	/*@Test
-	public void sould_return_X_when_10 (){
+	public void should_return_X_when_10 (){
 		
 	}*/
 }
